c17cd16f90498ec4714ce8843941f32805ffba05
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 14:30:03 -0700

    Create new componenets to support the integration of recommended Articles into the blog site.

---
+++ src/pages/ArticlesList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class ArticlesList extends Component {
+    render() {
+        <React.Fragment>    
+            <h1>These are some recommended articles from BlockCraft!</h1>
+            <p>
+                BlockCraft specializes in the implementation of the Accolade software suite 
+                and the development of supporting applications and plugins.  The majority 
+                of our development work is aimed at improving the user experience for Accolade
+                users, and by doing so improve the adoption of the software suite as a whole.
+            </p>
+        </React.Fragment>
+    }
+}
---
+++ src/pages/ArticlesPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class ArticlesPage extends Component {
+    render() {
+        <React.Fragment>    
+            <h1>This is a recommended Article!</h1>
+        </React.Fragment>
+    }
+}
