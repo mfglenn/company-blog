10f059867478ae9fcd5ec408d3015ad690a28cb0
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 16:03:05 -0700

    Integrate the article-content and the Article component class into the ArticlesList page to present a list of the available articles.

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
+import Article from '../components/Article'
+import articles from './article-content'
 
 export default class ArticlesList extends Component {
     render() {
         return(
             <React.Fragment>    
                 <h1>These are some recommended articles from BlockCraft!</h1>
-                <p>
-                    BlockCraft specializes in the implementation of the Accolade software suite 
-                    and the development of supporting applications and plugins.  The majority 
-                    of our development work is aimed at improving the user experience for Accolade
-                    users, and by doing so improve the adoption of the software suite as a whole.
-                </p>
+                {articles.map((article, i) => {
+                    return <Article key={i} article={article} />
+                })}
             </React.Fragment>
         )
     }
