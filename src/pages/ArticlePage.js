9b21f58ff4419d69c587f218e2f8e7b5d142f486
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 16:02:29 -0700

    Integrate the article content into the ArticlePage in order to support Query parameter lookups and return the queried article.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import Article from '../components/Article'
+import articles from './article-content'
 
 export default class ArticlePage extends Component {
     render() {
         //Deconstruct match property from props
         const { match } = this.props;
-
+        
         //Get query parameter name
         const name = match.params.name;
-
+        
+        //Lookup article
+        let article = articles.find(lkp => lkp.name === name);    
+        if (!article){
+            article = articles[0];
+        }
         return(
             <React.Fragment>    
-                <h1>This is the { name } article</h1>
+               <Article key={1} article={article} />
             </React.Fragment>
         )
     }
