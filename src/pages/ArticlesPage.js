2ce47278207108cf34e0a1a3deb21284ad01ea7f
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 14:39:38 -0700

    Correct render method return statements.

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 
 export default class ArticlesPage extends Component {
     render() {
-        <React.Fragment>    
-            <h1>This is a recommended Article!</h1>
-        </React.Fragment>
+        return(
+            <React.Fragment>    
+                <h1>This is a recommended Article!</h1>
+            </React.Fragment>
+        )
     }
 }
