97e2138aa447b88743c6d38e178b6eec370f4c9c
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 16:19:00 -0700

    Update ArtcilesList with redefined JSX.

--- src/pages/ArticlesList.js
+++ src/pages/ArticlesList.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+
 import Article from '../components/Article'
 import articles from './article-content'
 
@@ -6,9 +8,14 @@ export default class ArticlesList extends Component {
     render() {
         return(
             <React.Fragment>    
-                <h1>These are some recommended articles from BlockCraft!</h1>
+                <h1>Articles:</h1>
                 {articles.map((article, i) => {
-                    return <Article key={i} article={article} />
+                    return (
+                        <Link className="article-list-item" key={i} to={`/article/${article.name}`}>
+                            <h3>{article.title}</h3>
+                            <p>{article.content[0].substring(0, 150)}</p>
+                        </Link>
+                    )
                 })}
             </React.Fragment>
         )
