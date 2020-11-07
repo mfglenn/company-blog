b1caab5ac2be62c382c046ecada11ded4996c74d
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 21:42:13 -0700

    Modify the AddCommentForm component to post the new comments from the form.  Add the form to the ArticlePage to support the addition of new comments.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -4,13 +4,16 @@ import CommentsList from '../components/CommentsList';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 import UpvoteSection from '../components/UpvoteSection';
+import AddCommentForm from '../components/AddCommentForm';
 
 const ArticlePage = ({ match }) => {     
     //Get query parameter name
     const name = match.params.name;
     
+    //Set state variable and setter method
     const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
 
+    //Get article data from the server
     useEffect(() => {
         const fetchData = async () => {
             const result = await fetch(`/api/articles/${name}`);
@@ -36,6 +39,7 @@ const ArticlePage = ({ match }) => {
                 return (<p key={j}>{paragraph}</p>) 
             })}
             <CommentsList comments={articleInfo.comments}/>
+            <AddCommentForm articleName={name} setArticleInfo={setArticleInfo}/>
             <h3>Other Articles</h3>
             <ArticlesList articles={relatedArticles} /> 
         </React.Fragment>
