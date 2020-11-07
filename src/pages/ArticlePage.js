963a72e5238ebd34efa30ae1cf94365dbb7fd667
Matthew Glenn <mfglenn@outlook.com>
2020-11-06 20:47:47 -0700

    Update the ArticlePage to useState and useEffect to retrieve api data from the server. Update package.json to use the proxy of localhost:8000.

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,31 +1,41 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import Article from '../components/Article';
 import articles from './article-content';
 import NotFoundPage from './NotFoundPage';
 
-export default class ArticlePage extends Component {
-    render() {
-        //Deconstruct match property from props
-        const { match } = this.props;
-        
-        //Get query parameter name
-        const name = match.params.name;
-        
-        //Lookup article
-        let article = articles.find(lkp => lkp.name === name);    
-        
-        if (!article){
-            return (<NotFoundPage/>);
-        }
-        let relatedArticles = articles.filter(lkp => lkp.name !== name);
+const ArticlePage = ({ match }) => {     
+    //Get query parameter name
+    const name = match.params.name;
+    
+    const [articleInfo, setArticleInfo] = useState({upvotes: 0, comments: []});
 
-        return(
-            <React.Fragment>    
-               <Article key={1} article={article} />
-               <h3>Other Articles</h3>
-               <ArticlesList articles={relatedArticles} /> 
-            </React.Fragment>
-        )
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await fetch(`/api/articles/${name}`);
+            const body = await result.json();
+            setArticleInfo(body);
+        }
+        fetchData();
+    }, [name]);
+
+    //Lookup article
+    let article = articles.find(lkp => lkp.name === name);    
+    article.upvotes = articleInfo.upvotes;
+    article.comments = articleInfo.comments;
+
+    if (!article) {
+        return (<NotFoundPage/>);
     }
+    let relatedArticles = articles.filter(lkp => lkp.name !== name);
+
+    return(
+        <React.Fragment>    
+            <Article key={1} article={article} />
+            <h3>Other Articles</h3>
+            <ArticlesList articles={relatedArticles} /> 
+        </React.Fragment>
+    )
 }
+
+export default ArticlePage;
